9f72477e26fea74e24e945438e0294ad583ed65c
duongnghoang <hoangduongatwork@gmail.com>
2025-03-28 21:37:07 +0700

    remove redundant properties and cleanup

--- CarWebAPI/Middleware/InputValidateMiddleware.cs
+++ CarWebAPI/Middleware/InputValidateMiddleware.cs
@@ -1,112 +1,98 @@
-
 using System.Text.Json;
 using CarWebAPI.Helpers;
-using ICarManager = CarWebAPI.Interfaces.ICarManager;
-namespace Middleware
+
+namespace CarWebAPI.Middleware;
+
+public class InputValidateMiddleware
 {
-    public class InputValidateMiddleware
+    private readonly RequestDelegate _next;
+
+    public InputValidateMiddleware(RequestDelegate next) 
     {
-        private readonly RequestDelegate _next;
-        private readonly ILogger<InputValidateMiddleware> _logger;
-        public InputValidateMiddleware(RequestDelegate next, ILogger<InputValidateMiddleware> logger)
-        {
-            _next = next;
-            _logger = logger;
-        }
-        public async Task InvokeAsync(HttpContext context, ICarManager carManager)
-        {
-            try
-            {
-                context.Request.EnableBuffering();
-                if (context.Request.Method == "GET")
-                {
-                    await _next(context);
-                    return;
-                }
-                using var reader = new StreamReader(context.Request.Body);
-                var body = await reader.ReadToEndAsync();
-                context.Request.Body.Position = 0; // Reset lại stream
-                var jsonObject = JsonDocument.Parse(body);
-
-                // Validate id field
-                if (!ValidateHelpers.CheckId(jsonObject, out int id))
-                {
-                    await ValidateHelpers.ReturnError(context, "Id is required.");
-                    return;
-                }
-                if (carManager.GetById(id) != null)
-                {
-                    await ValidateHelpers.ReturnError(context, "Id already exists.");
-                    return;
-                }
-
-                // Validate make field
-                if (!ValidateHelpers.CheckRequired("make", jsonObject))
-                {
-                    await ValidateHelpers.ReturnError(context, "Make is required.");
-                    return;
-                }
-                // Validate model field
-                if (!ValidateHelpers.CheckRequired("model", jsonObject))
-                {
-                    await ValidateHelpers.ReturnError(context, "Model is required.");
-                    return;
-                }
-                // Validate year field
-                if (!ValidateHelpers.CheckRequired("year", jsonObject))
-                {
-                    await ValidateHelpers.ReturnError(context, "Year is required.");
-                    return;
-                }
-                // Validate year field range
-                if (!ValidateHelpers.CheckYear(jsonObject.RootElement.GetProperty("year").ToString()))
-                {
-                    await ValidateHelpers.ReturnError(context, "Invalid year value. Year must be between 1886 and current year.");
-                    return;
-                }
-                // Validate carType field
-                if (!ValidateHelpers.CheckRequired("carType", jsonObject))
-                {
-                    await ValidateHelpers.ReturnError(context, "CarType is required.");
-                    return;
-                }
-                // Validate carType field value
-                if (!ValidateHelpers.CheckCarType(jsonObject.RootElement.GetProperty("carType").ToString()))
-                {
-                    await ValidateHelpers.ReturnError(context, "Invalid carType value. CarType must be Fuel or Electric.");
-                    return;
-                }
-                // Validate lastMaintanenceTime field
-                if (!ValidateHelpers.CheckRequired("lastMaintanenceTime", jsonObject))
-                {
-                    await ValidateHelpers.ReturnError(context, "lastMaintanenceTime is required.");
-                    return;
-                }
-                // Validate lastMaintanenceTime field range
-                if (!ValidateHelpers.CheckMaintanenceTime(jsonObject.RootElement.GetProperty("lastMaintanenceTime").ToString()))
-                {
-                    await ValidateHelpers.ReturnError(context, "Invalid lastMaintanenceTime value. lastMaintanenceTime must be between 1886 and current year.");
-                    return;
-                }
-
-
-                await _next(context);
-            }
-            catch (Exception ex)
-            {
-                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
-                await context.Response.WriteAsync("Internal Server Error: " + ex.Message);
-            }
-
-        }
-    }
-    public static class InputValidateMiddlewareExtensions
-    {
-        public static IApplicationBuilder UseValidateMiddleware(
-            this IApplicationBuilder builder)
-        {
-            return builder.UseMiddleware<InputValidateMiddleware>();
-        }
+        _next = next;
     }
 
+    public async Task InvokeAsync(HttpContext context)
+    {
+        context.Request.EnableBuffering();
+        if (context.Request.Method == "GET" || context.Request.Method == "PUT")
+        {
+            await _next(context);
+            return;
+        }
+
+        using var reader = new StreamReader(context.Request.Body);
+        var body = await reader.ReadToEndAsync();
+        context.Request.Body.Position = 0; // Reset lại stream
+        var jsonObject = JsonDocument.Parse(body);
+
+        // Validate id field
+        if (!ValidateHelpers.CheckId(jsonObject, out var id))
+        {
+            await ValidateHelpers.ReturnError(context, "Id is required.");
+            return;
+        }
+
+        // Validate make field
+        if (!ValidateHelpers.CheckRequired("make", jsonObject))
+        {
+            await ValidateHelpers.ReturnError(context, "Make is required.");
+            return;
+        }
+
+        // Validate model field
+        if (!ValidateHelpers.CheckRequired("model", jsonObject))
+        {
+            await ValidateHelpers.ReturnError(context, "Model is required.");
+            return;
+        }
+
+        // Validate year field
+        if (!ValidateHelpers.CheckRequired("year", jsonObject))
+        {
+            await ValidateHelpers.ReturnError(context, "Year is required.");
+            return;
+        }
+
+        // Validate year field range
+        if (!ValidateHelpers.CheckYear(jsonObject.RootElement.GetProperty("year").ToString()))
+        {
+            await ValidateHelpers.ReturnError(context,
+                "Invalid year value. Year must be between 1886 and current year.");
+            return;
+        }
+
+        // Validate carType field
+        if (!ValidateHelpers.CheckRequired("carType", jsonObject))
+        {
+            await ValidateHelpers.ReturnError(context, "CarType is required.");
+            return;
+        }
+
+        // Validate carType field value
+        if (!ValidateHelpers.CheckCarType(jsonObject.RootElement.GetProperty("carType").ToString()))
+        {
+            await ValidateHelpers.ReturnError(context, "Invalid carType value. CarType must be Fuel or Electric.");
+            return;
+        }
+
+        // Validate lastMaintanenceTime field
+        if (!ValidateHelpers.CheckRequired("lastMaintenanceTime", jsonObject))
+        {
+            await ValidateHelpers.ReturnError(context, "lastMaintenanceTime is required.");
+            return;
+        }
+
+        // Validate lastMaintanenceTime field range
+        if (!ValidateHelpers.CheckMaintanenceTime(jsonObject.RootElement.GetProperty("lastMaintenanceTime")
+                .ToString()))
+        {
+            await ValidateHelpers.ReturnError(context,
+                "Invalid lastMaintenanceTime value. lastMaintenanceTime must be between 1886 and current year.");
+            return;
+        }
+
+
+        await _next(context);
+    }
 }
--- CarWebAPI/Middleware/LoggingMiddleware.cs
+++ CarWebAPI/Middleware/LoggingMiddleware.cs
@@ -1,37 +1,36 @@
 using System.Text;
 
-namespace CarWebAPI.Middleware
+namespace CarWebAPI.Middleware;
+
+public class LoggingMiddleware
 {
-    public class LoggingMiddleware
+    private readonly RequestDelegate _next;
+
+    public LoggingMiddleware(RequestDelegate next)
     {
-        private readonly RequestDelegate _next;
+        _next = next;
+    }
 
-        public LoggingMiddleware(RequestDelegate next)
+    public async Task Invoke(HttpContext context)
+    {
+        var request = context.Request;
+
+        request.EnableBuffering();
+
+        using (var reader = new StreamReader(request.Body, Encoding.UTF8, leaveOpen: true))
         {
-            _next = next;
+            var body = await reader.ReadToEndAsync();
+            request.Body.Position = 0;
+
+            var log = $"Schema: {request.Scheme}\n" +
+                      $"Host: {request.Host}\n" +
+                      $"Path: {request.Path}\n" +
+                      $"QueryString: {request.QueryString}\n" +
+                      $"Request Body: {body}\n\n";
+
+            await File.AppendAllTextAsync("logs.txt", log);
         }
 
-        public async Task Invoke(HttpContext context)
-        {
-            var request = context.Request;
-
-            request.EnableBuffering();
-
-            using (var reader = new StreamReader(request.Body, Encoding.UTF8, leaveOpen: true))
-            {
-                var body = await reader.ReadToEndAsync();
-                request.Body.Position = 0;
-
-                var log = $"Schema: {request.Scheme}\n" +
-                          $"Host: {request.Host}\n" +
-                          $"Path: {request.Path}\n" +
-                          $"QueryString: {request.QueryString}\n" +
-                          $"Request Body: {body}\n\n";
-
-                await File.AppendAllTextAsync("logs.txt", log);
-            }
-
-            await _next(context);
-        }
+        await _next(context);
     }
 }
