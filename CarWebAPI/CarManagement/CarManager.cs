f9c386e32c6e087a9ce7d98469ba9f10ca09cbab
duongnghoang <hoangduongatwork@gmail.com>
2025-03-28 14:14:59 +0700

    add car

--- CarWebAPI/CarManagement/CarManager.cs
+++ CarWebAPI/CarManagement/CarManager.cs
@@ -1,64 +1,63 @@
-using CarWebAPI.Common;
-using CarWebAPI.Interfaces;
+using CarWebAPI.DbContext;
 
 namespace CarWebAPI.CarManagement
 {
     public class CarManager
     {
-        private readonly 
+        private readonly IInMemoryDbContext _dbContext;
 
-        public CarManager()
+        public CarManager(IInMemoryDbContext dbContext)
         {
-            CarList = new List<Car>();
+            _dbContext = dbContext;
         }
 
         public void AddCar(Car car)
         {
-            CarList.Add(car);
+            _dbContext.Add(car);
         }
 
-        public void ViewAllCars()
-        {
-            DisplayCarList(CarList);
-        }
+        //public void ViewAllCars()
+        //{
+        //    DisplayCarList(CarList);
+        //}
 
-        public List<Car> SearchCarByMake(string make)
-        {
-            return CarList.Where(car => car.Make.Equals(make, StringComparison.OrdinalIgnoreCase)).ToList();
-        }
+        //public List<Car> SearchCarByMake(string make)
+        //{
+        //    return CarList.Where(car => car.Make.Equals(make, StringComparison.OrdinalIgnoreCase)).ToList();
+        //}
 
-        public List<Car> FilterCarsByType(CarType carType)
-        {
-            switch (carType)
-            {
-                case CarType.Fuel:
-                    return CarList.Where(car => car is IFuelable).ToList();
-                case CarType.Electric:
-                    return CarList.Where(car => car is IChargable).ToList();
-                default:
-                    return null;
-            }
-        }
+        //public List<Car> FilterCarsByType(CarType carType)
+        //{
+        //    switch (carType)
+        //    {
+        //        case CarType.Fuel:
+        //            return CarList.Where(car => car is IFuelable).ToList();
+        //        case CarType.Electric:
+        //            return CarList.Where(car => car is IChargable).ToList();
+        //        default:
+        //            return null;
+        //    }
+        //}
 
-        public string RemoveCarByModel(string model)
-        {
-            var carToRemove = CarList.FirstOrDefault(car => car.Model.Equals(model, StringComparison.OrdinalIgnoreCase));
-            if (carToRemove != null)
-            {
-                CarList.Remove(carToRemove);
-                return "Remove car successfully";
-            }
-            return "There is no car need to remove";
-        }
+        //public string RemoveCarByModel(string model)
+        //{
+        //    var carToRemove = CarList.FirstOrDefault(car => car.Model.Equals(model, StringComparison.OrdinalIgnoreCase));
+        //    if (carToRemove != null)
+        //    {
+        //        CarList.Remove(carToRemove);
+        //        return "Remove car successfully";
+        //    }
+        //    return "There is no car need to remove";
+        //}
 
-        public void DisplayCarList(List<Car> carList)
-        {
-            Console.WriteLine("Listing all the cars");
-            for (int i = 0; i < carList.Count(); i++)
-            {
-                Console.WriteLine($"Car No.{i}:");
-                carList[i].DisplayDetails();
-            }
-        }
+        //public void DisplayCarList(List<Car> carList)
+        //{
+        //    Console.WriteLine("Listing all the cars");
+        //    for (int i = 0; i < carList.Count(); i++)
+        //    {
+        //        Console.WriteLine($"Car No.{i}:");
+        //        carList[i].DisplayDetails();
+        //    }
+        //}
     }
 }
