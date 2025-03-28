a142cacb01f164004df7f4fa71fc940d0b7cb2cb
duongnghoang <hoangduongatwork@gmail.com>
2025-03-28 14:09:03 +0700

    add dbcontext

--- CarWebAPI/CarManagement/Car.cs
+++ CarWebAPI/CarManagement/Car.cs
@@ -2,6 +2,7 @@ namespace CarWebAPI.CarManagement
 {
     public abstract class Car
     {
+        public int Id { get; set; }
         public string Make { get; set; }
         public string Model { get; set; }
         public int Year { get; set; }
---
+++ CarWebAPI/DbContext/InMemoryDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Xml;
+using CarWebAPI.CarManagement;
+
+namespace CarWebAPI.DbContext;
+
+public class InMemoryDbContext
+{
+        private readonly List<Car> _entities = new List<Car>();
+
+        public void Add(Car entity)
+        {
+            _entities.Add(entity);
+        }
+
+        public IEnumerable<Car> GetAll()
+        {
+            return _entities;
+        }
+
+        public Car GetById(int id)
+        {
+            return _entities.FirstOrDefault(e => e.Id == id);
+        }
+}
