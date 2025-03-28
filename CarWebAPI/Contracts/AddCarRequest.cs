49c0aae7aaa912b1f122b41e8155bed18ad106a9
duongnghoang <hoangduongatwork@gmail.com>
2025-03-28 21:37:33 +0700

    fix language

--- CarWebAPI/Contracts/AddCarRequest.cs
+++ CarWebAPI/Contracts/AddCarRequest.cs
@@ -1,3 +1,3 @@
 ﻿namespace CarWebAPI.Contracts;
 
-public record AddCarRequest(int Id, string Make, string Model, int Year, string CarType, DateTime LastMaintanenceTime);
+public record AddCarRequest(int Id, string Make, string Model, int Year, string CarType, DateTime LastMaintenanceTime);
